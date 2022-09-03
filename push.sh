2dc698a7463df08ec99cb1b98c10c76fb91b5d01
zhen <zhen.yang@hotmail.fr>
2022-09-03 16:12:57 +0200

    maj

--- push.sh
+++ push.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 #push to github
 
-./pull.sh
-
+# git add -A 
+# git commit -m "maj"
+# git push origin master
 git add -A 
-git commit -m "maj"
+if [[ $1 = '' ]]
+then
+    git commit -m $1
+else
+    git commit -m "maj"
+fi
 git push origin master
